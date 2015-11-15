0d5a3ffb849d401338e3f651ed9558f12d91ebe8
Jon Green <jgreen82@gatech.edu>
2015-11-15 16:28:21 -0500

    Two hour test. 5 to 7 test is passing.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var prompt = require('prompt');
 var Babysitter = function() {};
 
 Babysitter.prototype.calculate = function(start, end) {
-	return 12;
+	return 12 * (end-start);
 };
 
 module.exports = Babysitter;
--- app_test.spec.js
+++ app_test.spec.js
@@ -7,4 +7,9 @@ describe('Babysitter nightly charge calculator', function() {
 		var result = babysitter.calculate(17,18); // military time
 		expect(result).toEqual(12);
 	});
+
+	it('Two hours of work from 5pm to 7pm', function() {
+		var result = babysitter.calculate(17,19);
+		expect(result).toEqual(24);
+	});
 });
